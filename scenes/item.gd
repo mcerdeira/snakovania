4eb95e4c6e157d9b84610136a22f7edcc5a04cc2
Martin Cerdeira <martincerdeira@gmail.com>
2026-02-03 13:04:45 -0300

    Mapa de mentira

--- scenes/item.gd
+++ scenes/item.gd
@@ -1,21 +1,36 @@
 extends Area2D
 @export var item_type = ""
+var title = ""
+var description = ""
 
 func _ready() -> void:
 	visible = false
 	$sprite.animation = item_type
-	if item_type == "grow" and Global.Hasgrow:
-		queue_free()
-	elif item_type == "map" and Global.HasMap:
-		queue_free()
-	elif item_type == "swim" and Global.HasSwim:
-		queue_free()
-	else:
-		visible = true
+	if item_type == "grow":
+		title = "GROW"
+		description = "Press <SPACE> to start growing and <ARROWS> to choose direction."
+		if Global.Hasgrow:
+			queue_free()
+		else:
+			visible = true
+	elif item_type == "map":
+		title = "MAP"
+		description = "Press <M> to see a map."
+		if Global.HasMap:
+			queue_free()
+		else:
+			visible = true
+	elif item_type == "swim":
+		title = "SWIM"
+		description = "Dive into the cold waters."
+		if Global.HasSwim:
+			queue_free()
+		else:
+			visible = true
 
 func _on_body_entered(body: Node2D) -> void:
 	if body and body.is_in_group("player"):
-		Global.ItemNotification.shownotif("GROW", "Press <SPACE> to start growing and <ARROWS> to choose direction")
+		Global.ItemNotification.shownotif(title, description)
 		if item_type == "grow":
 			Global.Hasgrow = true
 			
---
+++ scenes/map.gd
@@ -0,0 +1,66 @@
+extends Node2D
+var map_cells = []
+
+func _physics_process(delta: float) -> void:
+	if %CameraMap.enabled:
+		if Input.is_action_just_pressed("zoomin"):
+			%CameraMap.zoom += Vector2(1,1)
+		if Input.is_action_just_pressed("zoomout"):
+			%CameraMap.zoom -= Vector2(1,1)
+		if Input.is_action_pressed("right"):
+			%CameraMap.global_position.x += 10 * delta
+		if Input.is_action_pressed("left"):
+			%CameraMap.global_position.x -= 10 * delta
+	
+	if Input.is_action_just_pressed("map"):
+		%CameraMap.enabled = !%CameraMap.enabled
+		%Camera.enabled = !%Camera.enabled
+		if %CameraMap.enabled:
+			map_cells = []
+			draw_map()
+		else:
+			destroy_map()
+			
+func destroy_map():
+	for m in map_cells:
+		m.queue_free()
+		
+func draw_map():
+	const GRID_COLS = 16
+	const GRID_ROWS = 16
+
+	const VIEW_W = 1152.0
+	const VIEW_H = 640.0
+
+	const ROOM_W = 1152.0
+	const ROOM_H = 640.0
+
+	const CELL_W = VIEW_W / GRID_COLS
+	const CELL_H = VIEW_H / GRID_ROWS
+
+	const SCALE_X = CELL_W / ROOM_W
+	const SCALE_Y = CELL_H / ROOM_H
+
+	for y in range(Global.World.size()):
+		for x in range(Global.World[y].size()):
+			var cell = Global.World[y][x]
+			if cell == "-1":
+				continue
+
+			var room_name = "room_" + str(cell) + ".tscn"
+			var lvl_scene = load("res://levels/" + room_name)
+			if lvl_scene == null:
+				continue
+
+			var lvl = lvl_scene.instantiate()
+			add_child(lvl)
+			map_cells.append(lvl)
+
+			# Escala
+			lvl.scale = Vector2(SCALE_X, SCALE_Y)
+
+			# Posición en grilla
+			lvl.position = Vector2(
+				x * CELL_W,
+				y * CELL_H
+			)
